16f0414b4e6ff52e6ef6094d2f9178215573f958
Ken S <kksiu@wisc.edu>
2015-05-03 02:59:18 -0400

    Finally

--- src/install_spdy.sh
+++
@@ -1,5 +0,0 @@
-sudo dpkg -r mod-spdy-beta
-sudo rm -f out/Debug/mod-spdy-beta*.deb
-sudo BUILDTYPE=Debug make linux_package_deb
-sudo dpkg -i out/Debug/mod-spdy-beta*.deb
-sudo service apache2 restart
