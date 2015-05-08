379aeba96ce24453fd3da63a2070910f80669fae
Ken S <kksiu@wisc.edu>
2015-05-08 01:08:37 -0400

    install removes previous error log

--- src/install_spdy.sh
+++ src/install_spdy.sh
@@ -3,4 +3,5 @@ sudo rm -f out/Debug/mod-spdy-beta*.deb
 sudo rm -f out/Release/mod-spdy-beta*.deb
 sudo BUILDTYPE=Release make linux_package_deb
 sudo dpkg -i out/Release/mod-spdy-beta*.deb
+sudo rm -f /var/log/apache2/error.log
 sudo service apache2 restart
